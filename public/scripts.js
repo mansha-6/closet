312fb60239372d7a75d87cc2788fd6e0df94f644
Pranjallpandey1504 <pranjal1504pandey@gmail.com>
2025-02-06 10:29:15 +0530

    initial work

---
+++ public/scripts.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    setTimeout(() => {
+      document.getElementById("topClothing").style.top = "40%";
+      document.getElementById("bottomClothing").style.top = "50%";
+    }, 500);
+  };
+  
