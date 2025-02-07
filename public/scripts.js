4e86c96bae8eb1c16c437e331279399306ad9f3e
Pranjallpandey1504 <pranjal1504pandey@gmail.com>
2025-02-07 09:50:39 +0530

    updated

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,11 @@
 window.onload = () => {
-    setTimeout(() => {
-      document.getElementById("topClothing").style.top = "40%";
-      document.getElementById("bottomClothing").style.top = "50%";
-    }, 500);
-  };
-  
+  setTimeout(() => {
+    // Right Side Clothing Transition
+    document.getElementById("topClothing").style.top = "40%";
+    document.getElementById("bottomClothing").style.top = "50%";
+
+    // Left Side Clothing Transition
+    document.getElementById("topClothingLeft").style.top = "40%";
+    document.getElementById("bottomClothingLeft").style.top = "50%";
+  }, 500);
+};
